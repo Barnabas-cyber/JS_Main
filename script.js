7ffe4d85a64491d9daedf1c6c7dd1b80a2d47678
Barnbas-cyber <moloibarnabas@gmail.com>
2022-08-30 17:46:56 +0200

    functions implementations and testing

---
+++ script.js
@@ -0,0 +1,13 @@
+//JS rock, paper or scissors game
+
+//computer function
+function computerPlay(rock, paper, scissors) {
+    let result;
+    result = rock, paper, scissors;
+    return result;
+}
+
+//single round game function
+function singleRound(playerSelection,computerSelection) {
+
+}
